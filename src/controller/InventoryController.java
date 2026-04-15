45d5f886f527c6f97e40f127e78dd1c836e1feef
Jerrykabwende <jkabwende@ggc.edu>
2026-04-14 23:07:04 -0400

    created InventoryController class, Item, and Item.txt Jerry

---
+++ src/controller/InventoryController.java
@@ -0,0 +1,8 @@
+package controller;
+import model.Item;
+import java.util.ArrayList;
+
+public class InventoryController {
+    private ArrayList<Item> inventory;
+    private int maxSize;
+}
---
+++ src/model/Item.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Item {
+    private String name;
+    private String itemType;
+    private String rarity;
+    private String description;
+    private String benefit;
+    private String weakness;
+
+    private int healAmount;
+    private int attackBonus;
+    private int defenseBonus;
+    private int missChance;
+
+    private boolean equippable;
+    private boolean consumable;
+    private String keyTarget;
+}
